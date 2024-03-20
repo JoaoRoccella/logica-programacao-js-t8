9e4296706b2e55d6788fe751d97cf44c572f7121
Joao Roccella <roccella@gmail.com>
2024-03-20 12:37:12 +0000

    Dicas desafio 3

---
+++ desafio3.js
@@ -0,0 +1,13 @@
+// const temperatura = parseFloat(window.prompt('Digite a temperatura'));
+// const escalaAtual = window.prompt('Digite a escala atual').toUpperCase();
+
+console.log(`O valor não é NaN: ${!isNaN(temperatura)}`);
+
+// parseFloat() -> FUNÇÃO que converte texto para tipo number com ponto decimal
+// parseInt()   ->  FUNÇÃO que converte texto para tipo number inteiro
+// toUpperCase() -> METODO que converte o texto em maiúsculas
+// toLowerCase() -> MÉTODO que converte o texto em minúsculas
+
+// window.alert(`A temperatura convertida é ${temperatura + 273.15}`)
+// console.log(typeof (temperatura));
+
--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,9 +1,12 @@
-const a = false, b = false;
+// texto.toUpperCase(); -> transforma o texto em maiúscula
+// texto.toLowerCase(); -> transforma em minúscula
 
-if (a || b) {
-    console.log('VERDADEIRO');
-} else {
-    console.log('FALSO');
+const e1 = 'C'.toUpperCase(), e2 = 'f'.toUpperCase;
+
+if (e1 == 'C' && e2 == 'K') {
+    console.log('Celsius -> Kelvin');
+} else if (e1 == 'C' && e2 == 'F')  {
+    console.log('Celsius -> Fahrenheit');
 }
 
 // && -> E
