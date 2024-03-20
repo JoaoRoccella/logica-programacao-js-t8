956819f74dd16b7c78b7a7f61bb8a9569b542c44
JoaoRoccella <roccella@gmail.com>
2024-03-20 11:37:15 -0300

    Queimando cérebros

--- desafio3.js
+++ desafio3.js
@@ -1,13 +1,62 @@
-// const temperatura = parseFloat(window.prompt('Digite a temperatura'));
-// const escalaAtual = window.prompt('Digite a escala atual').toUpperCase();
+const temperaturaAtual = parseFloat(window.prompt('Digite a temperatura'));
 
-console.log(`O valor não é NaN: ${!isNaN(temperatura)}`);
+if (!isNaN(temperaturaAtual)) {
 
-// parseFloat() -> FUNÇÃO que converte texto para tipo number com ponto decimal
-// parseInt()   ->  FUNÇÃO que converte texto para tipo number inteiro
-// toUpperCase() -> METODO que converte o texto em maiúsculas
-// toLowerCase() -> MÉTODO que converte o texto em minúsculas
+    const
+        escalaAtual = window.prompt('Digite a escala atual: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase(),
+        
+        escalaConversao = window.prompt('Digite a escala para conversão: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase();
 
-// window.alert(`A temperatura convertida é ${temperatura + 273.15}`)
-// console.log(typeof (temperatura));
+    if (escalaAtual === 'C') {
+
+        if (escalaConversao === 'K') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual + 273.15} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'F') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual * 9 / 5) + 32} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'C') {
+            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+
+        } else {
+            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+        }
+
+    } else if (escalaAtual === 'F') {
+
+        if (escalaConversao === 'K') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual + 459.67) * 5 / 9} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'C') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual - 32) * 5 / 9} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'F') {
+            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+
+        } else {
+            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+        }
+
+    } else if (escalaAtual === 'K') {
+
+        if (escalaConversao === 'C') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual - 273.15} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'F') {
+            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual * 9 / 5 - 459.67} em ${escalaConversao}`);
+
+        } else if (escalaConversao === 'K') {
+            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+
+        } else {
+            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+        }
+
+    } else {
+        window.alert(`Escala selecionada: ${escalaAtual}. A escala ATUAL não existe ou não está disponível neste programa.`);
+    }
+
+} else {
+    window.alert('O número digitado para conversão não é um número válido.');
+}
 
