aada9ad63e4226d15eb73a464b87e0474383a9eb
JoaoRoccella <roccella@gmail.com>
2024-03-21 10:45:13 -0300

    Transformando os desafios em funções

--- desafio3.js
+++ desafio3.js
@@ -1,62 +1,67 @@
-const temperaturaAtual = parseFloat(window.prompt('Digite a temperatura'));
+function converteTemperatura() {
 
-if (!isNaN(temperaturaAtual)) {
-
-    const
-        escalaAtual = window.prompt('Digite a escala atual: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase(),
-        
-        escalaConversao = window.prompt('Digite a escala para conversão: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase();
-
-    if (escalaAtual === 'C') {
-
-        if (escalaConversao === 'K') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual + 273.15} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'F') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual * 9 / 5) + 32} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'C') {
-            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
-
+    const temperaturaAtual = parseFloat(window.prompt('Digite a temperatura'));
+    
+    if (!isNaN(temperaturaAtual)) {
+    
+        const
+            escalaAtual = window.prompt('Digite a escala atual: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase(),
+            
+            escalaConversao = window.prompt('Digite a escala para conversão: \nC -> Celsius, F -> Fahrenheit, K -> Kelvin').toUpperCase();
+    
+        if (escalaAtual === 'C') {
+    
+            if (escalaConversao === 'K') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual + 273.15} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual * 9 / 5) + 32} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'C') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
+        } else if (escalaAtual === 'F') {
+    
+            if (escalaConversao === 'K') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual + 459.67) * 5 / 9} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'C') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual - 32) * 5 / 9} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
+        } else if (escalaAtual === 'K') {
+    
+            if (escalaConversao === 'C') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual - 273.15} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'F') {
+                window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual * 9 / 5 - 459.67} em ${escalaConversao}`);
+    
+            } else if (escalaConversao === 'K') {
+                window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    
+            } else {
+                window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            }
+    
         } else {
-            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
+            window.alert(`Escala selecionada: ${escalaAtual}. A escala ATUAL não existe ou não está disponível neste programa.`);
         }
-
-    } else if (escalaAtual === 'F') {
-
-        if (escalaConversao === 'K') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual + 459.67) * 5 / 9} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'C') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${(temperaturaAtual - 32) * 5 / 9} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'F') {
-            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
-
-        } else {
-            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
-        }
-
-    } else if (escalaAtual === 'K') {
-
-        if (escalaConversao === 'C') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual - 273.15} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'F') {
-            window.alert(`${temperaturaAtual} em ${escalaAtual} -> ${temperaturaAtual * 9 / 5 - 459.67} em ${escalaConversao}`);
-
-        } else if (escalaConversao === 'K') {
-            window.alert(`Escala selecionada: ${escalaConversao}. Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
-
-        } else {
-            window.alert(`Escala selecionada: ${escalaConversao}. Essa escala não existe ou não está disponível neste programa.`);
-        }
-
+    
     } else {
-        window.alert(`Escala selecionada: ${escalaAtual}. A escala ATUAL não existe ou não está disponível neste programa.`);
+        window.alert('O número digitado para conversão não é um número válido.');
     }
 
-} else {
-    window.alert('O número digitado para conversão não é um número válido.');
 }
+
 
---
+++ funcoes.js
@@ -0,0 +1,12 @@
+// Funções
+
+function somaDoisNumeros(num1, num2) {
+    const soma = num1 + num2;
+    return soma;
+}
+
+const media = somaDoisNumeros(2, 3) / 2;
+const media2 = somaDoisNumeros(56, media) / 3;
+
+console.log(media);
+console.log(media2);
