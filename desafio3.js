4b187d352a993fce73b2a8adab7948c20aae9b8b
JoaoRoccella <roccella@gmail.com>
2024-04-01 11:46:55 -0300

    Mostrando erros no conversor de temperatura

--- desafio3.js
+++ desafio3.js
@@ -1,11 +1,15 @@
 function converteTemperatura(temperaturaAtual, escalaAtual, escalaConversao) {
 
-    let temperaturaConvertida, msgErro = 'Ok';
+    let
+        temperaturaConvertida,
+        msgErro = 'Ok',
+        grauEscala = escalaConversao !== 'K' ? true : false;
     
-    if (!isNaN(temperaturaAtual)) {
+    if (!isNaN(temperaturaAtual) && temperaturaAtual.length) {
     
         escalaAtual = escalaAtual.toUpperCase();
         escalaConversao = escalaConversao.toUpperCase();
+        temperaturaAtual = Number(temperaturaAtual);
     
         if (escalaAtual === 'C') {
     
@@ -66,6 +70,7 @@ function converteTemperatura(temperaturaAtual, escalaAtual, escalaConversao) {
         escalaAtual: escalaAtual,
         escalaConversao: escalaConversao,
         temperaturaConvertida: temperaturaConvertida,
+        grauEscala: grauEscala,
         msgErro: msgErro
     } 
 
