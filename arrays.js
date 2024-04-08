d11f4de60acfbf6917b62cf2feb1d582fe33d8c0
JoaoRoccella <roccella@gmail.com>
2024-04-08 11:45:43 -0300

    Trabalhando com arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// índices         0    1    2    3    4    5    6
+const variavel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+for (let i = 0; i < variavel.length; i++) {
+
+    console.log(variavel[i]);
+
+}
+
+// indice = posição -1
